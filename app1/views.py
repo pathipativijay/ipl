6e04edac25172d1e5fdc68b8e2b399e2aa39b398
vijaypathipati <vijaypathipati19165@gmail.com>
2022-03-17 18:29:16 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+# Create your views here.
+def rutraj(request):
+    return HttpResponse("rutraj is good consentcy player in csk")
